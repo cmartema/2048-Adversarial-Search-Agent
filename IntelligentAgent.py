743be17851752d40d786677ca938112bd3d5d290
cmartema <cjm2301@columbia.edu>
2023-10-31 14:57:20 -0400

    Progress_v2

--- IntelligentAgent.py
+++ IntelligentAgent.py
@@ -2,57 +2,61 @@ import random
 from BaseAI import BaseAI
 class IntelligentAgent(BaseAI): 
     def getMove(self, grid):
-        # Selects a random move and returns it
         moveset = grid.getAvailableMoves()
-        return random.choice(moveset) if moveset else None
-    
-    #Constants for directions
-    ACTIONS = {
-        'UP': 0,
-        'DOWN': 1,
-        'LEFT': 2,
-        'RIGHT': 3
-    }
-    
-    def minimize(self, state, a, b, depth):
-        '''Find the child state with the lowest utility value'''
-        if self.terminal_test(state) or depth == 0:
-            return(None, self.evaluate(state))
-        
-        minChild = None 
-        minUtility = float("inf")
+        best_move = None
+        best_utility = float("-inf")
+        alpha = float("-inf")
+        beta = float("inf")
 
-        for child in state.children:
-            _, utility = self.maximize(child, a, b, depth - 1)
+        for move in moveset:
+            child = grid.clone()
+            child.move(move)
+            utility = self.expectiminimax(child, 3, False, alpha, beta)
 
-            if utility < minUtility:
-                minChild = child
-                minUtility = utility
+            if utility > best_utility:
+                best_move = move
+                best_utility = utility
 
-            if minUtility <= a:
-                break
-            if minUtility < b:
-                b = minUtility
-        return (minChild, minUtility) 
+            alpha = max(alpha, utility)
 
-    def maximize(self, state, a, b, depth):
-        '''Find the child state with the highest utility value'''
-        if self.terminal_test(state) or depth == 0:
-            return(None, self.evaluate(state))
+        return best_move
 
-        maxChild = None
-        maxUtility = float("-inf")
+    def expectiminimax(self, grid, depth, maximizing_player, alpha, beta):
+        if depth == 0 or not self.areMovesAvailable(grid):
+            return self.evaluate(grid)
 
-        for child in state.children:
-            _, utility = self.minimize(child, a, b, depth - 1)
-            if utility > maxUtility:
-                maxChild = child
-                maxUtility = utility
-            if maxUtility >= b:
-                break
-            if maxUtility > a:
-                a = maxUtility
-            return (maxChild, maxUtility)
+        if maximizing_player:
+            max_utility = float("-inf")
+            for move in grid.getAvailableMoves():
+                child = grid.clone()
+                child.move(move)
+                utility = self.expectiminimax(child, depth - 1, False, alpha, beta)
+                max_utility = max(max_utility, utility)
+                alpha = max(alpha, utility)
+                if beta <= alpha:
+                    break
+            return max_utility
+        else:
+            min_utility = 0
+            num_children = 0
+            for cell in grid.getAvailableCells():
+                child2 = grid.clone()
+                child2.setCellValue(cell, 2)
+                min_utility += 0.9 * self.expectiminimax(child2, depth - 1, True, alpha, beta)
+                num_children += 1
+                child4 = grid.clone()
+                child4.setCellValue(cell, 4)
+                min_utility += 0.1 * self.expectiminimax(child4, depth - 1, True, alpha, beta)
+                num_children += 1
+            return min_utility / num_children
+
+    def evaluate(self, grid):
+        pass
+
+    def areMovesAvailable(self, grid):
+        for move in grid.getAvailableMoves():
+            return True
+        return False        
 
 
             
