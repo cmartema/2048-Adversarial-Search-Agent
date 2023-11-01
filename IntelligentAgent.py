1603af7c4e285304ba3d58817e4e3c5cdb99f5ec
cmartema <cjm2301@columbia.edu>
2023-10-31 23:32:52 -0400

    issue with finishing early

--- IntelligentAgent.py
+++ IntelligentAgent.py
@@ -1,14 +1,14 @@
 import random
 from BaseAI import BaseAI
 class IntelligentAgent(BaseAI): 
-    def getMove(self, grid):
-        moveset = grid.getAvailableMoves()
+    def getMove(self, grid):   
         best_move = None
         best_utility = float("-inf")
         alpha = float("-inf")
         beta = float("inf")
 
-        for move in moveset:
+        for move_tuple in  grid.getAvailableMoves():
+            move = move_tuple[0]
             child = grid.clone()
             child.move(move)
             utility = self.expectiminimax(child, 3, False, alpha, beta)
@@ -18,8 +18,9 @@ class IntelligentAgent(BaseAI):
                 best_utility = utility
 
             alpha = max(alpha, utility)
-
+    
         return best_move
+    
 
     def expectiminimax(self, grid, depth, maximizing_player, alpha, beta):
         if depth == 0 or not self.areMovesAvailable(grid):
@@ -37,26 +38,66 @@ class IntelligentAgent(BaseAI):
                     break
             return max_utility
         else:
-            min_utility = 0
+            expected_utility = 0.0
             num_children = 0
             for cell in grid.getAvailableCells():
-                child2 = grid.clone()
-                child2.setCellValue(cell, 2)
-                min_utility += 0.9 * self.expectiminimax(child2, depth - 1, True, alpha, beta)
-                num_children += 1
-                child4 = grid.clone()
-                child4.setCellValue(cell, 4)
-                min_utility += 0.1 * self.expectiminimax(child4, depth - 1, True, alpha, beta)
-                num_children += 1
-            return min_utility / num_children
+                for tile_value, probability in [(2, 0.9), (4, 0.1)]:
+                    child = grid.clone()
+                    child.setCellValue(cell, tile_value)
+                    expected_utility += probability * self.expectiminimax(child, depth - 1, True, alpha, beta)
+                    num_children += 1
+                    if beta <= alpha:
+                        break
+            return expected_utility / num_children
 
     def evaluate(self, grid):
-        pass
+        smooth_weight = 0.1
+        monotonic_weight = 1.0
+
+        smoothness = self.calculate_smoothness(grid)
+        monotonicity = self.calculate_monotonicity(grid)
+        max_tile = grid.getMaxTile()
+        empty_cells = len(grid.getAvailableCells())
+
+        # Calculate the overall heuristic value
+        heuristic = (
+            monotonic_weight * monotonicity
+            - smooth_weight * smoothness
+            + max_tile
+            + empty_cells
+        )
+        return heuristic
+    
+    def calculate_smoothness(self, grid):
+        smoothness = 0.0
+        for row in grid.map:
+            smoothness += sum(abs(row[i] - row[i + 1]) for i in range(len(row) - 1))
+        for col in range(len(grid.map[0])):
+            smoothness += sum(abs(grid.map[row][col] - grid.map[row + 1][col]) for row in range(len(grid.map) - 1))
+
+        # Normalize the smoothness heuristic
+        smoothness /= (len(grid.map) * len(grid.map[0]) - 1)
+        return smoothness  
+
+    def calculate_monotonicity(self, grid):
+        monotonicity = 0.0
+        for row in grid.map:
+            for i in range(len(row) - 1):
+                monotonicity += abs(row[i] - row[i + 1])
+        for col in range(len(grid.map[0])):
+            for row in range(len(grid.map) - 1):
+                monotonicity += abs(grid.map[row][col] - grid.map[row + 1][col])
+
+        # Normalize the monotonicity heuristic
+        monotonicity /= (len(grid.map) * len(grid.map[0]))
+        return monotonicity
 
     def areMovesAvailable(self, grid):
         for move in grid.getAvailableMoves():
             return True
         return False        
+
+
 
 
             
